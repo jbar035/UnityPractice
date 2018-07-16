2bfca44da5ed8ea9df8e54a18f7562688737dd1b
Jake Barnes <jakebarnes@jakes-mbp.home>
2018-07-16 20:29:20 +0100

    Added more audio, fixed gameplay issues

---
+++ 3_Boost_Game/Don't Crash/Assets/Scenes/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource rocketSound;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Rotate();
+        Thrust();
+    }
+
+    private void Rotate(){
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketSound.isPlaying)
+            {
+                rocketSound.Play();
+            }
+
+        }
+        else rocketSound.Stop();
+    }
+}
