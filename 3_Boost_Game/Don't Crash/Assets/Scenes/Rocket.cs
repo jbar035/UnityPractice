5505dbf58ca2f1ede6f415814822c99f8c1d9717
Jake Barnes <jakebarnes@Jakes-MacBook-Pro.local>
2018-07-18 18:42:01 +0100

    Added collision functionality

--- 3_Boost_Game/Don't Crash/Assets/Scenes/Rocket.cs
+++ 3_Boost_Game/Don't Crash/Assets/Scenes/Rocket.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainEngineThrust = 100f;
     Rigidbody rigidBody;
     AudioSource rocketSound;
 
@@ -19,24 +20,47 @@ public class Rocket : MonoBehaviour {
         Thrust();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag){
+            case "Friendly":
+                //do nothing
+                print("Friendly");
+                break;
+            case "Fuel":
+                //add fuel
+                print("Collected fuel!");
+                break;
+            case "Finish":
+                //win level
+                print("Level complete!");
+                break;
+            default:
+                print("Game over");
+                break;
+        }
+    }
+
     private void Rotate(){
         rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 
     private void Thrust()
     {
+        float thrustThisFrame = mainEngineThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!rocketSound.isPlaying)
             {
                 rocketSound.Play();
